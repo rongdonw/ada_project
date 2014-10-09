a00b27d754fe78c61e090c4c32c411931ff76f7d
rongdonw <rongdong@rongdongs-air.home>
2014-10-09 16:46:03 -0400

    prepare data
    10/09/2014

--- ada_1009.R
+++ ada_1009.R
@@ -1,6 +1,23 @@
+#######Read, prepare data###################
 d.train<-read.csv("training.csv",header = TRUE,sep=",",stringsAsFactors=F)
 d.test<-read.csv("test.csv",header = TRUE,sep=",",stringsAsFactors=F)
 str(d.train)
 im.train<- d.train$Image
 d.train$Image <- NULL
 head(d.train)
+#as.integer(unlist(strsplit(im.train[1], " ")))
+#install.packages('doMC')
+#library(doMC)
+#registerDoMC()
+im.train <- foreach(im = im.train, .combine=rbind) %dopar% {
+  as.integer(unlist(strsplit(im, " ")))
+}
+str(im.train)
+im.test <- foreach(im = d.test$Image, .combine=rbind) %dopar% {
+  as.integer(unlist(strsplit(im, " ")))
+}
+d.test$Image <- NULL
+save(d.train, im.train, d.test, im.test, file='data.Rd')
+load('data.Rd')
+
+#############
